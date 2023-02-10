7273e60f7efc7f5310a6a1cf042b38a0813a6ff8
mazzdriver <oleg@massdriver95.ru>
2023-02-10 13:53:34 +0300

    8 kyu Keep Hydrated!

--- practice.py
+++ practice.py
@@ -1,3 +1,15 @@
+# # 8 kyu Keep Hydrated!
+# Nathan loves cycling.
+# Because Nathan knows it is important to stay hydrated, he
+# drinks 0.5 litres of water per hour of cycling.
+# You get given the time in hours and you need to return the
+# number of litres Nathan will drink, rounded to the smallest value.
+
+
+def litres(time):
+    return time // 2
+
+
 # # 8 kyu MakeUpperCase
 # Write a function which converts the input string to uppercase.
 

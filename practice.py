704a3aab1e26ac4e5f8437663286de85d43dec7c
mazzdriver <oleg@massdriver95.ru>
2023-02-10 13:45:18 +0300

    8 kyu Returning Strings

--- practice.py
+++ practice.py
@@ -1,5 +1,19 @@
+# # 8 kyu Returning Strings
+# Make a function that will return a greeting statement
+# that uses an input; your program should return,
+# "Hello, <name> how are you doing today?".
 
-# 7 kyu Digit*Digit
+
+def greet(name):            # my first solution
+    # Good Luck (like you need it)
+    return 'Hello, ' + str(name) + ' how are you doing today?'
+
+
+def cw_greet(name):         # enhanced, looks so beauty
+    return f'Hello, {name} how are you doing today?'
+
+# # 7 kyu Digit*Digit
+
 
 def square_digits(num):     # my solution
     # Your code here
@@ -9,5 +23,5 @@ def square_digits(num):     # my solution
     return int(output)
 
 
-def square_digits(num):     # less strings
+def cw_square_digits(num):     # less strings
     return int(''.join(str(int(d)**2) for d in str(num)))

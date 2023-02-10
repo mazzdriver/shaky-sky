bd7422ebc858328e4d3093317c6340d5537399a8
mazzdriver <oleg@massdriver95.ru>
2023-02-10 14:29:39 +0300

    6 kyu Detect Pangram

--- practice.py
+++ practice.py
@@ -1,3 +1,25 @@
+# # 6 kyu Detect Pangram
+# A pangram is a sentence that contains every single letter of
+# the alphabet at least once. For example, the sentence
+# "The quick brown fox jumps over the lazy dog" is a pangram,
+# because it uses the letters A-Z at least once (case is irrelevant).
+# Given a string, detect whether or not it is a pangram.
+# Return True if it is, False if not.
+# Ignore numbers and punctuation.
+
+
+def is_pangram(s: str):     # my solution
+    alphabet = 'qwertyuiopasdfghjklzxcvbnm'
+    for c in alphabet:
+        if c not in s.lower():
+            return False
+    return True
+
+
+def cw_is_pangram(s):       # not my solution, but looks good
+    return set('qwertyuiopasdfghjklzxcvbnm').issubset(s.lower())
+
+
 # # 7 kyu Disemvowel Trolls
 # Trolls are attacking your comment section!
 # A common way to deal with this situation is to remove all
